55a7df713c47479e530cb26c8c104105c5074241
aaronburmeister <f34r91@gmail.com>
2020-06-10 17:07:22 -0600

    Add username to nav bar

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,9 +1,11 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking'
 import { inject as service } from '@ember/service'
 
 export default class NavBarComponent extends Component {
     @service authManager
+    @tracked username = window.localStorage.username
 
     @action
     logout() {
